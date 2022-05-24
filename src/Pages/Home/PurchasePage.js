12d1a846edbb3260b0ddd6a08ebbbf58f27d58cc
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-05-24 10:17:43 +0600

    finished purchase page

--- src/Pages/Home/PurchasePage.js
+++ src/Pages/Home/PurchasePage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const PurchasePage = () => {
@@ -10,8 +9,6 @@ const PurchasePage = () => {
     const { purchasePageId } = useParams();
 
     const [purchase, setPurchase] = useState({});
-    // const quantity = inventory.quantity
-    // console.log(quantity)
     useEffect(() => {
         const url = `http://localhost:5000/tool/${purchasePageId}`
         fetch(url)
@@ -22,31 +19,33 @@ const PurchasePage = () => {
     ///////////////////////////////
     const handleAddItem = event => {
         event.preventDefault();
-        const  order = event.target.ratings.value;
-        const addItem = {
+        const  order = event.target.order.value;
+        const minimumQuantity= purchase.minimumQuantity
+        const availableQuantity = purchase.availableQuantity
+        const orders = {
             name: user.displayName,
             email: user.email,
-            description: event.target.description.value,
+            toolName: event.target.toolName.value,
+            Address: event.target.Address.value,
             order: event.target.order.value,
+            phone: event.target.phone.value,
         }
+        if(order >= minimumQuantity && order <= availableQuantity ){
+        fetch('http://localhost:5000/order',{
+            method:'POST',
+            headers:{'content-type':'application/json'},
+            body:JSON.stringify(orders)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            toast('Order is successful')
+        })
+    }
+    else{
+        toast('Order is not valid!!!')
         
-        if(order > -1 && order <6 ){
-            axios.post('http://localhost:5000/', addItem)
-
-            .then(Response => {
-                const { data } = Response;
-                if (data.insertedId) {
-                    console.log(data)
-                    
-                    toast('Review is added!!!')
-                    event.target.reset();
-                }
-            })
-        }
-        else{
-            toast('ratings is not valid!!!')
-            
-        }
+    }
 
     }
     return (
@@ -58,6 +57,7 @@ const PurchasePage = () => {
                 <h2 class="card-title">{purchase.name}</h2>
                 <p>{purchase.description}</p>
                 <p>Price: {purchase.price}</p>
+                {/* <p>Order: {purchase.order}</p> */}
                 <h1>Minimum Quantity : <span className='text-xl text-secondary font-bold'>{purchase.minimumQuantity}</span></h1>
                 <h1>available Quantity : <span className='text-xl text-secondary font-bold'>{purchase.availableQuantity}</span></h1>
             </div>
@@ -68,11 +68,16 @@ const PurchasePage = () => {
                     <div class="max-w-md">
                         <h2 className='text-xl text-primary font-bold'>Order A Tool</h2>
                         <form onSubmit={handleAddItem} className='register'>
-                            <input type="text" name="name" value={user?.displayName} placeholder="Type here" class="input input-bordered w-full max-w-xs my-3" />
-                            <input type="email" value={user?.email} name="email" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                            <textarea class="textarea textarea-bordered w-full max-w-xs my-2" name="description" placeholder="Write Description"></textarea>
-                            <input className='input input-bordered w-full max-w-xs mb-3' type="number" name="order" placeholder='Order Quantity' id="" /><br />                            
-                            <input className='w-full max-w-xs text-primary uppercase font-bold' type="submit" value="Order" />
+                            <input type="text" name="toolName" value={purchase.name} placeholder="Type here" class="input input-bordered w-full max-w-xs mb-1" />
+                            <input type="text" name="name" value={user?.displayName} placeholder="Type here" class="input input-bordered w-full max-w-xs mb-1" />
+                            <input type="email" value={user?.email} name="email" placeholder="Type here" class="input input-bordered w-full max-w-xs mb-1" />
+                            <textarea class="textarea textarea-bordered w-full max-w-xs mb-1" name="Address" placeholder="Address"></textarea>
+
+                            <input type="text" name="price" value= {`price for one ${purchase.price}`} placeholder="Type here" class="input input-bordered w-full max-w-xs mb-1" />
+
+                            <input className='input input-bordered w-full max-w-xs mb-1' type="number" name="order" placeholder={`Minimum Order ${purchase.minimumQuantity} and Maximum Order ${purchase.availableQuantity}`} id="" /><br />                            
+                            <input className='input input-bordered w-full max-w-xs mb-1' type="number" name="phone" placeholder='phone' id="" /><br />                            
+                            <input className='w-full max-w-xs text-primary uppercase font-bold mb-1' type="submit" value="Click Here To Order" />
                              
                         </form>
                         
