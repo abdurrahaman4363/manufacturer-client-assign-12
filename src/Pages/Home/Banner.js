d437490b5c1754ef2f32b47fccda77844d2ce63e
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2024-03-14 20:07:55 +0600

    added semicolon

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
     return (
         <div class="carousel w-full">
         <div id="slide1" class="carousel-item relative w-full">
-          <img style={{height:'500px'}} src={banner1} alt="" class="w-full"/>
+          <img style={{height:'500px;'}} src={banner1} alt="" class="w-full"/>
           <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" class="btn btn-circle">❮</a> 
             <a href="#slide2" class="btn btn-circle">❯</a>
